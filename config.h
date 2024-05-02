38f5e96a2d5a7004655bad896445755949ac8984
Georgi Tarashev <georgitarashev@gmail.com>
2024-05-02 18:52:07 +0100

    Updated font and added a gitignore

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "JetBrainsMono:size=14", "AppleColorEmoji:pixelsize=16:antialias=true:autohint=true"  };
+static char *fonts[]          = { "Oxygen Mono:size=14", "AppleColorEmoji:pixelsize=16:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222"; // red
 static char normbordercolor[]       = "#444444"; // gray
 static char normfgcolor[]           = "#bbbbbb"; // light gray
